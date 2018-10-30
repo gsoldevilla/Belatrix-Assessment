d513ca80c6ef24827295c709153b3020079c3719
[ACN]-[MR]-[Jorge Gonzalez] <jorge.a.soldevilla@accenture.com>
2018-10-30 13:52:59 -0500

    Prueba Belatrix
    Se encapsula el código de validacion en métodos específicos.

--- pruebaBelatrix/src/main/java/org/springframework/pruebaBelatrix/JobLogger.java
+++ pruebaBelatrix/src/main/java/org/springframework/pruebaBelatrix/JobLogger.java
@@ -1,6 +1,7 @@
 package org.springframework.pruebaBelatrix;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -24,6 +25,9 @@ public class JobLogger {
 	private static Map<String, String> dbParams;
 	private static Logger logger;
 	private static Connection conexion;
+	private static final int ID_LOG_MESSAGE = 1;
+	private static final int ID_LOG_ERROR = 2;
+	private static final int ID_LOG_WARNING = 3;
 
 	public JobLogger(boolean logToFileParam, boolean logToConsoleParam, boolean logToDatabaseParam,
 			boolean logMessageParam, boolean logWarningParam, boolean logErrorParam, Map<String, String> dbParamsMap) {
@@ -47,55 +51,11 @@ public class JobLogger {
 				throw new Exception("Error or Warning or Message must be specified");
 			}
 
-			int t = 0;
-			if (message && logMessage) {
-				t = 1;
-			}
+			generarLogs(messageText, message, warning, error);
+			System.out.println("Se generaron los logs satisfactoriamente");
 
-			if (error && logError) {
-				t = 2;
-			}
-
-			if (warning && logWarning) {
-				t = 3;
-			}
-
-			
-
-			String l = null;
-			File logFile = new File(dbParams.get("logFileFolder") + "/logFile.txt");
-			if (!logFile.exists()) {
-				logFile.createNewFile();
-			}
-
-			FileHandler fh = new FileHandler(dbParams.get("logFileFolder") + "/logFile.txt");
-			ConsoleHandler ch = new ConsoleHandler();
-
-			if (error && logError) {
-				l = l + "error " + DateFormat.getDateInstance(DateFormat.LONG).format(new Date()) + messageText;
-			}
-
-			if (warning && logWarning) {
-				l = l + "warning " + DateFormat.getDateInstance(DateFormat.LONG).format(new Date()) + messageText;
-			}
-
-			if (message && logMessage) {
-				l = l + "message " + DateFormat.getDateInstance(DateFormat.LONG).format(new Date()) + messageText;
-			}
-
-			if (logToFile) {
-				logger.addHandler(fh);
-				logger.log(Level.INFO, messageText);
-			}
-
-			if (logToConsole) {
-				logger.addHandler(ch);
-				logger.log(Level.INFO, messageText);
-			}
-			
-			
-
-			
+		} else {
+			System.out.println("Error en el formato del mensaje!!!"); 
 		}
 
 	}
@@ -118,24 +78,81 @@ public class JobLogger {
 			connectionProps.put("user", dbParams.get("userName"));
 			connectionProps.put("password", dbParams.get("password"));
 
-			try {				
-				conexion = DriverManager.getConnection("jdbc:" + dbParams.get("dbms") + "://" + dbParams.get("serverName")
-						+ ":" + dbParams.get("portNumber") + "/", connectionProps);
+			try {
+				conexion = DriverManager.getConnection("jdbc:" + dbParams.get("dbms") + "://"
+						+ dbParams.get("serverName") + ":" + dbParams.get("portNumber") + "/", connectionProps);
 			} catch (SQLException e) {
 				e.printStackTrace();
-				System.out.println("Hubo un error en la conexion con la Base de Datos");				
-			}			
+				System.out.println("Hubo un error en la conexion con la Base de Datos");
+			}
 		}
 		return conexion;
 	}
-	
+
 	public static void registrarenBD(String mensaje, int tipoMensaje) throws SQLException {
-		if (getConexionBD()!=null) {
+		if (getConexionBD() != null) {
 			Statement stmt = getConexionBD().createStatement();
 			stmt.executeUpdate("insert into Log_Values('" + mensaje + "', " + String.valueOf(tipoMensaje) + ")");
 			System.out.println("Mensaje registrado en la BD");
 		} else {
 			System.out.println("No se pudo registrar el mensaje en la BD");
 		}
+	}
+
+	public static void generarLogs(String mensaje, boolean message, boolean warning, boolean error)
+			throws SecurityException, IOException, SQLException {
+		String l = "";
+		if (error && logError) {
+			l = l + "error " + DateFormat.getDateInstance(DateFormat.LONG).format(new Date()) + mensaje;
+		}
+
+		if (warning && logWarning) {
+			l = l + "warning " + DateFormat.getDateInstance(DateFormat.LONG).format(new Date()) + mensaje;
+		}
+
+		if (message && logMessage) {
+			l = l + "message " + DateFormat.getDateInstance(DateFormat.LONG).format(new Date()) + mensaje;
+		}
+
+		mensaje = l;
+
+		if (logToFile) {
+			logger.addHandler(getFileHandler());
+			logger.log(Level.INFO, mensaje);
+		}
+
+		if (logToConsole) {
+			logger.addHandler(getConsoleHandler());
+			logger.log(Level.INFO, mensaje);
+		}
+
+		if (logToDatabase) {
+			if (message && logMessage) {
+				registrarenBD(mensaje, ID_LOG_MESSAGE);
+			}
+
+			if (error && logError) {
+				registrarenBD(mensaje, ID_LOG_ERROR);
+			}
+
+			if (warning && logWarning) {
+				registrarenBD(mensaje, ID_LOG_WARNING);
+			}
+
+		}
+	}
+
+	public static FileHandler getFileHandler() throws IOException {
+		File logFile = new File(dbParams.get("logFileFolder") + "/logFile.txt");
+		if (!logFile.exists()) {
+			logFile.createNewFile();
+		}
+		FileHandler fh = new FileHandler(dbParams.get("logFileFolder") + "/logFile.txt");
+		return fh;
+	}
+
+	public static ConsoleHandler getConsoleHandler() throws IOException {
+		ConsoleHandler ch = new ConsoleHandler();
+		return ch;
 	}
 }
